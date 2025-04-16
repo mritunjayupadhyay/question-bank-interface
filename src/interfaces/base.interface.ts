7f0a19d86b9559452b92cdc4ad6f70fc311ffa97
mritunjayupadhyay <mupadhyay00@gmail.com>
2025-04-16 23:15:27 +0700

    feat: add IlabelValue

--- src/interfaces/base.interface.ts
+++ src/interfaces/base.interface.ts
@@ -2,3 +2,10 @@ export interface IBaseResponse {
     error: boolean;
     message: string;
 }
+
+export interface ILabelValue {
+    label: string;
+    value: string;
+    isSelected?: boolean;
+    icon?: string;
+}
