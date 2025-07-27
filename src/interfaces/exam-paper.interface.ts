48d5aa39e69942e86c63f98850f0404f979bf500
mritunjayupadhyay <mupadhyay00@gmail.com>
2025-07-27 23:40:11 +0700

    feat: add type

--- src/interfaces/exam-paper.interface.ts
+++ src/interfaces/exam-paper.interface.ts
@@ -1,13 +1,15 @@
 export interface IExamPaper {
-    classId: string;
     createdAt: string; // ISO 8601 date string
     durationMinutes: number;
     examTypeId: string;
     id: string;
-    subjectId: string;
     title: string;
     totalMarks: number;
     updatedAt: string; // ISO 8601 date string
+}
+
+export interface IExamPaperWithType extends IExamPaper {
+    examTypeName: string;
 }
 
 export interface IExamPaperSection { }
