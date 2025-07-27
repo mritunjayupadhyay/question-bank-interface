6fe52a255b3855e6636ffcd9bf24789b5f115f32
mritunjayupadhyay <mupadhyay00@gmail.com>
2025-07-27 23:31:50 +0700

    feat: add exam paper list item

--- src/interfaces/exam-paper.interface.ts
+++ src/interfaces/exam-paper.interface.ts
@@ -1,3 +1,13 @@
-export interface IExamPaper {}
+export interface IExamPaper {
+    classId: string;
+    createdAt: string; // ISO 8601 date string
+    durationMinutes: number;
+    examTypeId: string;
+    id: string;
+    subjectId: string;
+    title: string;
+    totalMarks: number;
+    updatedAt: string; // ISO 8601 date string
+}
 
-export interface IExamPaperSection {}
+export interface IExamPaperSection { }
